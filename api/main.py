0310e4c382e4fb6f6144f8e3e7a970072e601840
SnowDu <3292957@qq.com>
2023-11-29 23:19:44 +0800

    改代码

--- api/main.py
+++ api/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def run(server_class=HTTPServer, handler_class=Handler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
 
-if __name__ == '__main__':
-    app.run()
+run()
