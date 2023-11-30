3c8a3e565af15aa79dcae4cd0dbd8d536ac502c0
SnowDu <3292957@qq.com>
2023-12-01 01:40:38 +0800

    222

--- api/index.py
+++ api/index.py
@@ -7,31 +7,15 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-	if request.method =="GET":
-		result = "test"
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write("DearXuan's API by python!".encode())
+		
+		self.wfile.write(openai.api_key.encode())
+        return
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
