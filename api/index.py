f530392b5b131ed9336ee3619ab9e0ff86f15c1e
SnowDu <3292957@qq.com>
2023-12-08 22:47:03 +0800

    aaaa

--- api/index.py
+++ api/index.py
@@ -7,24 +7,29 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class handler(BaseHTTPRequestHandler):
+    conversation_history = [
+        {"role": "system", "content": "你是一个非常厉害的工作助手，你无所不能"},
+    ]
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write("DearXuan's API by python!".encode())
-        prompt = "请将这句话翻译中英语“山有多高，地有多险，人心就有多高”"
-        response = self.get_gpt3_response(prompt)
+        #self.wfile.write("DearXuan's API by python!".encode())
+        prompt = "请作诗一首"
+        response = self.send_message(prompt)
         self.wfile.write(response.encode())
         return
 
-    def get_gpt3_response(self, prompt):
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=100
-        )
-        return response.choices[0].text.strip()
+    def send_message(self, message):
+        self.conversation_history.append({"role": "user", "content": message})
 
-    def GetNewStrFromNstr(self, instr):
-        return "12345"
+        response = openai.ChatCompletion.create(
+            model="gpt-4-1106-preview",
+            messages=self.conversation_history
+        )
+
+        assistant_reply = response['choices'][0]['message']['content']
+        self.conversation_history.append({"role": "assistant", "content": assistant_reply})
+
+        return assistant_reply
